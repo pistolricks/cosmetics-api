6864ef79b61880b1d627260fcb76a7924826223a
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-08-09 19:25:35 -0700

    made the session return the original if it exists in th db

--- internal/riman/sessions.go
+++ internal/riman/sessions.go
@@ -5,6 +5,7 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"time"
 
 	"github.com/pistolricks/kbeauty-api/internal/validator"
@@ -12,6 +13,10 @@ import (
 
 const (
 	ScopeAuthentication = "authentication"
+)
+
+var (
+	ErrDuplicateToken = errors.New("duplicate Token")
 )
 
 type Session struct {
@@ -56,13 +61,45 @@ func generateRimanSession(clientID int64, ttl time.Duration, scope string, plain
 }
 
 func (m SessionModel) NewRimanSession(clientID int64, ttl time.Duration, scope string, plainText string, cartKey string, data map[string]any) (*Session, error) {
+
 	token, err := generateRimanSession(clientID, ttl, scope, plainText, cartKey, data)
 	if err != nil {
 		return nil, err
 	}
 
-	err = m.Insert(token)
-	return token, err
+	isSaved, session, err := m.SessionCheck(token)
+
+	if isSaved {
+		return session, nil
+	} else {
+		err = m.Insert(token)
+		return token, err
+	}
+}
+
+func (m SessionModel) SessionCheck(session *Session) (bool, *Session, error) {
+	queryCheck := `
+        SELECT hash, client_id, expiry, scope, cart_key, data
+        FROM sessions
+        WHERE hash = $1`
+
+	var checkedSession Session
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, queryCheck, session.Hash).Scan(&checkedSession.Hash, &checkedSession.ClientID, &checkedSession.Expiry, &checkedSession.Scope, &checkedSession.CartKey, &checkedSession.Data)
+
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return true, &checkedSession, ErrDuplicateToken
+		default:
+			return true, session, err
+		}
+	}
+
+	return false, session, nil
 }
 
 func (m SessionModel) Insert(session *Session) error {
