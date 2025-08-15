c9b9cd7bcea2bd9fded372b1be6527ea7cb3da8a
Ollivr <66511791+ollivr@users.noreply.github.com>
2025-08-15 12:42:48 -0700

    removed generated GraphQL schema file from repository

--- internal/v2/orders.go
+++ internal/v2/orders.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strings"
 
 	graphify "github.com/vinhluan/go-shopify-graphql"
 )
@@ -14,7 +15,7 @@ type OrderV2 struct {
 }
 
 func (v2 OrderV2) Orders() {
-	
+
 	orders, err := v2.Client.Order.ListAll(context.Background())
 	if err != nil {
 		panic(err)
@@ -23,4 +24,424 @@ func (v2 OrderV2) Orders() {
 	for _, c := range orders {
 		fmt.Println(c)
 	}
+}
+
+//go:generate mockgen -destination=./mock/order_service.go -package=mock . OrderService
+type OrderService interface {
+	Get(ctx context.Context, id graphql.ID) (*model.Order, error)
+
+	List(ctx context.Context, opts ListOptions) ([]*model.Order, error)
+	ListAll(ctx context.Context) ([]*model.Order, error)
+
+	ListAfterCursor(ctx context.Context, opts ListOptions) ([]*model.Order, *string, *string, error)
+
+	Update(ctx context.Context, input model.OrderInput) error
+}
+
+type OrderServiceOp struct {
+	client *Client
+}
+
+var _ OrderService = &OrderServiceOp{}
+
+type mutationOrderUpdate struct {
+	OrderUpdateResult struct {
+		UserErrors []model.UserError `json:"userErrors,omitempty"`
+	} `graphql:"orderUpdate(input: $input)" json:"orderUpdate"`
+}
+
+const orderBaseQuery = `
+	id
+	legacyResourceId
+	name
+	createdAt
+	customer{
+		id
+		legacyResourceId
+		firstName
+		displayName
+		email
+	}
+	clientIp
+	shippingAddress{
+		address1
+		address2
+		city
+		province
+		country
+		zip
+	}
+	shippingLine{
+		originalPriceSet{
+			presentmentMoney{
+				amount
+				currencyCode
+			}
+			shopMoney{
+				amount
+				currencyCode
+			}
+		}
+		title
+	}
+	taxLines{
+		priceSet{
+			presentmentMoney{
+				amount
+				currencyCode
+			}
+			shopMoney{
+				amount
+				currencyCode
+			}
+		}
+		rate
+		ratePercentage
+		title
+	}
+	totalReceivedSet{
+		presentmentMoney{
+			amount
+			currencyCode
+		}
+		shopMoney{
+			amount
+			currencyCode
+		}
+	}
+	note
+	tags
+	transactions {
+		processedAt
+		status
+		kind
+		test
+		amountSet {
+			shopMoney {
+				amount
+				currencyCode
+			}
+		}
+	}
+`
+
+const orderLightQuery = `
+	id
+	legacyResourceId
+	name
+	createdAt
+	customer{
+		id
+		legacyResourceId
+		firstName
+		displayName
+		email
+	}
+	shippingAddress{
+		address1
+		address2
+		city
+		province
+		country
+		zip
+	}
+	shippingLine{
+		title
+	}
+	totalReceivedSet{
+		shopMoney{
+			amount
+		}
+	}
+	note
+	tags
+`
+
+const lineItemFragment = `
+fragment lineItem on LineItem {
+	id
+	sku
+	quantity
+	fulfillableQuantity
+	fulfillmentStatus
+	product{
+		id
+		legacyResourceId
+	}
+	vendor
+	title
+	variantTitle
+	variant{
+		id
+		legacyResourceId
+		selectedOptions{
+			name
+			value
+		}
+	}
+	originalTotalSet{
+		presentmentMoney{
+			amount
+			currencyCode
+		}
+		shopMoney{
+			amount
+			currencyCode
+		}
+	}
+	originalUnitPriceSet{
+		presentmentMoney{
+			amount
+			currencyCode
+		}
+		shopMoney{
+			amount
+			currencyCode
+		}
+	}
+	discountedUnitPriceSet{
+		presentmentMoney{
+			amount
+			currencyCode
+		}
+		shopMoney{
+			amount
+			currencyCode
+		}
+	}
+	discountedTotalSet{
+		presentmentMoney{
+			amount
+			currencyCode
+		}
+		shopMoney{
+			amount
+			currencyCode
+		}
+	}
+}
+`
+
+const lineItemFragmentLight = `
+fragment lineItem on LineItem {
+	id
+	sku
+	quantity
+	fulfillableQuantity
+	fulfillmentStatus
+	vendor
+	title
+	variantTitle
+}
+`
+
+func (s *OrderServiceOp) Get(ctx context.Context, id graphql.ID) (*model.Order, error) {
+	q := fmt.Sprintf(`
+		query order($id: ID!) {
+			node(id: $id){
+				... on Order {
+					%s
+					lineItems(first:50){
+						edges{
+							node{
+								...lineItem
+							}
+						}
+					}
+					fulfillmentOrders(first:5){
+						edges {
+							node {
+								id
+								status
+								lineItems(first:50){
+									edges {
+										node {
+											id
+											remainingQuantity
+											totalQuantity
+											lineItem{
+												sku
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		%s
+	`, orderBaseQuery, lineItemFragment)
+
+	vars := map[string]interface{}{
+		"id": id,
+	}
+
+	out := struct {
+		Order *model.Order `json:"node"`
+	}{}
+	err := s.client.QueryString(ctx, q, vars, &out)
+	if err != nil {
+		return nil, fmt.Errorf("query: %w", err)
+	}
+
+	return out.Order, nil
+}
+
+func (s *OrderServiceOp) List(ctx context.Context, opts ListOptions) ([]*model.Order, error) {
+	q := fmt.Sprintf(`
+		{
+			orders(query: "$query"){
+				edges{
+					node{
+						%s
+						lineItems{
+							edges{
+								node{
+									...lineItem
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		%s
+	`, orderBaseQuery, lineItemFragment)
+
+	q = strings.ReplaceAll(q, "$query", opts.Query)
+
+	res := []*model.Order{}
+	err := s.client.BulkOperation.BulkQuery(ctx, q, &res)
+	if err != nil {
+		return nil, fmt.Errorf("bulk query: %w", err)
+	}
+
+	return res, nil
+}
+
+func (s *OrderServiceOp) ListAll(ctx context.Context) ([]*model.Order, error) {
+	q := fmt.Sprintf(`
+		{
+			orders(query: "$query"){
+				edges{
+					node{
+						%s
+						lineItems{
+							edges{
+								node{
+									...lineItem
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		%s
+	`, orderBaseQuery, lineItemFragment)
+
+	res := []*model.Order{}
+	err := s.client.BulkOperation.BulkQuery(ctx, q, &res)
+	if err != nil {
+		return nil, fmt.Errorf("bulk query: %w", err)
+	}
+
+	return res, nil
+}
+
+func (s *OrderServiceOp) ListAfterCursor(ctx context.Context, opts ListOptions) ([]*model.Order, *string, *string, error) {
+	q := fmt.Sprintf(`
+		query orders($query: String, $first: Int, $last: Int, $before: String, $after: String, $reverse: Boolean) {
+			orders(query: $query, first: $first, last: $last, before: $before, after: $after, reverse: $reverse){
+				edges{
+					node{
+						%s
+
+						lineItems(first:25){
+							edges{
+								node{
+									...lineItem
+								}
+							}
+						}
+					}
+					cursor
+				}
+				pageInfo{
+					hasNextPage
+				}
+			}
+		}
+
+		%s
+	`, orderLightQuery, lineItemFragmentLight)
+
+	vars := map[string]interface{}{
+		"query":   opts.Query,
+		"reverse": opts.Reverse,
+	}
+
+	if opts.After != "" {
+		vars["after"] = opts.After
+	} else if opts.Before != "" {
+		vars["before"] = opts.Before
+	}
+
+	if opts.First > 0 {
+		vars["first"] = opts.First
+	} else if opts.Last > 0 {
+		vars["last"] = opts.Last
+	}
+
+	out := struct {
+		Orders struct {
+			Edges []struct {
+				OrderQueryResult *model.Order `json:"node,omitempty"`
+				Cursor           string       `json:"cursor,omitempty"`
+			} `json:"edges,omitempty"`
+			PageInfo struct {
+				HasNextPage bool `json:"hasNextPage,omitempty"`
+			} `json:"pageInfo,omitempty"`
+		} `json:"orders,omitempty"`
+	}{}
+	err := s.client.QueryString(ctx, q, vars, &out)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("query: %w", err)
+	}
+
+	res := []*model.Order{}
+	var firstCursor *string
+	var lastCursor *string
+	if len(out.Orders.Edges) > 0 {
+		firstCursor = &out.Orders.Edges[0].Cursor
+		lastCursor = &out.Orders.Edges[len(out.Orders.Edges)-1].Cursor
+		for _, o := range out.Orders.Edges {
+			res = append(res, o.OrderQueryResult)
+		}
+	}
+
+	return res, firstCursor, lastCursor, nil
+}
+
+func (s *OrderServiceOp) Update(ctx context.Context, input model.OrderInput) error {
+	m := mutationOrderUpdate{}
+
+	vars := map[string]interface{}{
+		"input": input,
+	}
+	err := s.client.Mutate(ctx, &m, vars)
+	if err != nil {
+		return fmt.Errorf("mutation: %w", err)
+	}
+
+	if len(m.OrderUpdateResult.UserErrors) > 0 {
+		return fmt.Errorf("%+v", m.OrderUpdateResult.UserErrors)
+	}
+
+	return nil
 }
--- internal/v2/products.go
+++ internal/v2/products.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strings"
 
 	graphify "github.com/vinhluan/go-shopify-graphql"
+	"github.com/vinhluan/go-shopify-graphql/model"
 )
 
 type ProductV2 struct {
@@ -23,4 +25,391 @@ func (v2 ProductV2) Collections() {
 	for _, c := range collections {
 		fmt.Println(c.Handle)
 	}
+}
+
+//go:generate mockgen -destination=./mock/product_service.go -package=mock . ProductService
+type ProductService interface {
+	List(ctx context.Context, query string) ([]*model.Product, error)
+	ListAll(ctx context.Context) ([]*model.Product, error)
+
+	Get(ctx context.Context, id string) (*model.Product, error)
+
+	Create(ctx context.Context, product model.ProductInput, media []model.CreateMediaInput) (*model.Product, error)
+	Update(ctx context.Context, product model.ProductInput) (*model.Product, error)
+	Delete(ctx context.Context, product model.ProductDeleteInput) error
+
+	VariantsBulkCreate(ctx context.Context, id string, input []model.ProductVariantsBulkInput) error
+	VariantsBulkUpdate(ctx context.Context, id string, input []model.ProductVariantsBulkInput) error
+	VariantsBulkReorder(ctx context.Context, id string, input []model.ProductVariantPositionInput) error
+}
+
+type ProductServiceOp struct {
+	client *Client
+}
+
+var _ ProductService = &ProductServiceOp{}
+
+type mutationProductCreate struct {
+	ProductCreateResult model.ProductCreatePayload `graphql:"productCreate(input: $input, media: $media)" json:"productCreate"`
+}
+
+type mutationProductUpdate struct {
+	ProductUpdateResult model.ProductUpdatePayload `graphql:"productUpdate(input: $input)" json:"productUpdate"`
+}
+
+type mutationProductDelete struct {
+	ProductDeleteResult model.ProductDeletePayload `graphql:"productDelete(input: $input)" json:"productDelete"`
+}
+
+type mutationProductVariantsBulkCreate struct {
+	ProductVariantsBulkCreateResult struct {
+		UserErrors []model.UserError `json:"userErrors,omitempty"`
+	} `graphql:"productVariantsBulkCreate(productId: $productId, variants: $variants)" json:"productVariantsBulkCreate"`
+}
+
+type mutationProductVariantsBulkUpdate struct {
+	ProductVariantsBulkUpdateResult struct {
+		UserErrors []model.UserError `json:"userErrors,omitempty"`
+	} `graphql:"productVariantsBulkUpdate(productId: $productId, variants: $variants)" json:"productVariantsBulkUpdate"`
+}
+
+type mutationProductVariantsBulkReorder struct {
+	ProductVariantsBulkReorderResult struct {
+		UserErrors []model.UserError `json:"userErrors,omitempty"`
+	} `graphql:"productVariantsBulkReorder(positions: $positions, productId: $productId)" json:"productVariantsBulkReorder"`
+}
+
+const productBaseQuery = `
+	id
+	legacyResourceId
+	handle
+	options{
+		name
+		values
+		position
+	}
+	tags
+	title
+	description
+	descriptionPlainSummary
+	priceRangeV2{
+		minVariantPrice{
+			amount
+			currencyCode
+		}
+		maxVariantPrice{
+			amount
+			currencyCode
+		}
+	}
+	productType
+	vendor
+	totalInventory
+	onlineStoreUrl
+	descriptionHtml
+	seo{
+		description
+		title
+	}
+	templateSuffix
+	customProductType
+	featuredImage{
+		id
+		altText
+		height
+		width
+		url
+	}
+`
+
+var productQuery = fmt.Sprintf(`
+	%s
+	variants(first:250, after: $cursor){
+		edges{
+			node{
+				id
+				legacyResourceId
+				title
+				displayName
+				sku
+				selectedOptions{
+					name
+					value
+				}
+				position
+				image {
+					id
+					altText
+					height
+					width
+					url
+				}
+				compareAtPrice
+				price
+				inventoryQuantity
+				inventoryItem{
+					id
+					legacyResourceId
+				}
+				availableForSale
+			}
+		}
+		pageInfo{
+			hasNextPage
+		}
+	}
+`, productBaseQuery)
+
+var productBulkQuery = fmt.Sprintf(`
+	%s
+	metafields{
+		edges{
+			node{
+				id
+				legacyResourceId
+				namespace
+				key
+				value
+				type
+			}
+		}
+	}
+	variants{
+		edges{
+			node{
+				id
+				legacyResourceId
+				title
+				displayName
+				sku
+				selectedOptions{
+					name
+					value
+				}
+				position
+				image {
+					id
+					altText
+					height
+					width
+					url
+				}
+				compareAtPrice
+				price
+				inventoryQuantity
+				inventoryItem{
+					id
+					legacyResourceId
+				}
+				availableForSale
+			}
+		}
+	}
+`, productBaseQuery)
+
+func (s *ProductServiceOp) ListAll(ctx context.Context) ([]*model.Product, error) {
+	q := fmt.Sprintf(`
+		{
+			products{
+				edges{
+					node{
+						%s
+					}
+				}
+			}
+		}
+	`, productBulkQuery)
+
+	res := []*model.Product{}
+	err := s.client.BulkOperation.BulkQuery(ctx, q, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (s *ProductServiceOp) List(ctx context.Context, query string) ([]*model.Product, error) {
+	q := fmt.Sprintf(`
+		{
+			products(query: "$query"){
+				edges{
+					node{
+						%s
+					}
+				}
+			}
+		}
+	`, productBulkQuery)
+
+	q = strings.ReplaceAll(q, "$query", query)
+
+	res := []*model.Product{}
+	err := s.client.BulkOperation.BulkQuery(ctx, q, &res)
+	if err != nil {
+		return nil, fmt.Errorf("bulk query: %w", err)
+	}
+
+	return res, nil
+}
+
+func (s *ProductServiceOp) Get(ctx context.Context, id string) (*model.Product, error) {
+	out, err := s.getPage(ctx, id, "")
+	if err != nil {
+		return nil, err
+	}
+
+	nextPageData := out
+	hasNextPage := out.Variants.PageInfo.HasNextPage
+	for hasNextPage && len(nextPageData.Variants.Edges) > 0 {
+		cursor := nextPageData.Variants.Edges[len(nextPageData.Variants.Edges)-1].Cursor
+		nextPageData, err := s.getPage(ctx, id, cursor)
+		if err != nil {
+			return nil, fmt.Errorf("get page: %w", err)
+		}
+		out.Variants.Edges = append(out.Variants.Edges, nextPageData.Variants.Edges...)
+		hasNextPage = nextPageData.Variants.PageInfo.HasNextPage
+	}
+
+	return out, nil
+}
+
+func (s *ProductServiceOp) getPage(ctx context.Context, id string, cursor string) (*model.Product, error) {
+	q := fmt.Sprintf(`
+		query product($id: ID!, $cursor: String) {
+			product(id: $id){
+				%s
+			}
+		}
+	`, productQuery)
+
+	vars := map[string]interface{}{
+		"id": id,
+	}
+	if cursor != "" {
+		vars["cursor"] = cursor
+	}
+
+	out := struct {
+		Product *model.Product `json:"product"`
+	}{}
+	err := s.client.QueryString(ctx, q, vars, &out)
+	if err != nil {
+		return nil, fmt.Errorf("query: %w", err)
+	}
+
+	return out.Product, nil
+}
+
+func (s *ProductServiceOp) Create(ctx context.Context, product model.ProductInput, media []model.CreateMediaInput) (*model.Product, error) {
+	m := mutationProductCreate{}
+
+	vars := map[string]interface{}{
+		"input": product,
+		"media": media,
+	}
+
+	err := s.client.Mutate(ctx, &m, vars)
+	if err != nil {
+		return nil, fmt.Errorf("mutation: %w", err)
+	}
+
+	if len(m.ProductCreateResult.UserErrors) > 0 {
+		return nil, fmt.Errorf("%+v", m.ProductCreateResult.UserErrors)
+	}
+
+	return m.ProductCreateResult.Product, nil
+}
+
+func (s *ProductServiceOp) Update(ctx context.Context, product model.ProductInput) (*model.Product, error) {
+	m := mutationProductUpdate{}
+
+	vars := map[string]interface{}{
+		"input": product,
+	}
+	err := s.client.Mutate(ctx, &m, vars)
+	if err != nil {
+		return nil, fmt.Errorf("mutation: %w", err)
+	}
+
+	if len(m.ProductUpdateResult.UserErrors) > 0 {
+		return nil, fmt.Errorf("%+v", m.ProductUpdateResult.UserErrors)
+	}
+
+	return m.ProductUpdateResult.Product, nil
+}
+
+func (s *ProductServiceOp) Delete(ctx context.Context, product model.ProductDeleteInput) error {
+	m := mutationProductDelete{}
+
+	vars := map[string]interface{}{
+		"input": product,
+	}
+	err := s.client.Mutate(ctx, &m, vars)
+	if err != nil {
+		return fmt.Errorf("mutation: %w", err)
+	}
+
+	if len(m.ProductDeleteResult.UserErrors) > 0 {
+		return fmt.Errorf("%+v", m.ProductDeleteResult.UserErrors)
+	}
+
+	return nil
+}
+
+func (s *ProductServiceOp) VariantsBulkCreate(ctx context.Context, id string, input []model.ProductVariantsBulkInput) error {
+	m := mutationProductVariantsBulkCreate{}
+
+	vars := map[string]interface{}{
+		"productId": id,
+		"variants":  input,
+	}
+	err := s.client.Mutate(ctx, &m, vars)
+	if err != nil {
+		return fmt.Errorf("mutation: %w", err)
+	}
+
+	if len(m.ProductVariantsBulkCreateResult.UserErrors) > 0 {
+		return fmt.Errorf("%+v", m.ProductVariantsBulkCreateResult.UserErrors)
+	}
+
+	return nil
+}
+
+func (s *ProductServiceOp) VariantsBulkUpdate(ctx context.Context, id string, input []model.ProductVariantsBulkInput) error {
+	m := mutationProductVariantsBulkUpdate{}
+
+	vars := map[string]interface{}{
+		"productId": id,
+		"variants":  input,
+	}
+	err := s.client.Mutate(ctx, &m, vars)
+	if err != nil {
+		return fmt.Errorf("mutation: %w", err)
+	}
+
+	if len(m.ProductVariantsBulkUpdateResult.UserErrors) > 0 {
+		return fmt.Errorf("%+v", m.ProductVariantsBulkUpdateResult.UserErrors)
+	}
+
+	return nil
+}
+
+func (s *ProductServiceOp) VariantsBulkReorder(ctx context.Context, id string, input []model.ProductVariantPositionInput) error {
+	m := mutationProductVariantsBulkReorder{}
+
+	vars := map[string]interface{}{
+		"productId": id,
+		"positions": input,
+	}
+	err := s.client.Mutate(ctx, &m, vars)
+	if err != nil {
+		return fmt.Errorf("mutation: %w", err)
+	}
+
+	if len(m.ProductVariantsBulkReorderResult.UserErrors) > 0 {
+		return fmt.Errorf("%+v", m.ProductVariantsBulkReorderResult.UserErrors)
+	}
+
+	return nil
 }
